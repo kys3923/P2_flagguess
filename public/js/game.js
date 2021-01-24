81566aac1de9893357c83bf7bbc247eca09b822c
kys3923 <kys3923@gmail.com>
2021-01-23 20:01:13 -0500

    stuck on displaying svg images

--- public/js/game.js
+++ public/js/game.js
@@ -1,35 +1,35 @@
-const express = require('express');
-const router = express.Router()
+// const express = require('express');
+// const router = express.Router()
 
-let questionArray = ['one', 'two', 'three', 'four'];
-// loop
-// Push
-// questionArray.push({restCountry});
+// let questionArray = ['one', 'two', 'three', 'four'];
+// // loop
+// // Push
+// // questionArray.push({restCountry});
 
 
-// shuffle the array to ask questions
-function randomFlag(questionArray) {
-    let m = questionArray.length, t, i;
-    while (i < m) {
-        i = Math.floor(Math.random() * m--);
-        t = questionArray[m];
-        questionArray[m] = questionArray[i];
-        questionArray[i] = t;
-    }
-    return questionArray;
-}
-console.log(randomFlag(questionArray), "this is shuffled array");
+// // shuffle the array to ask questions
+// function randomFlag(questionArray) {
+//     let m = questionArray.length, t, i;
+//     while (i < m) {
+//         i = Math.floor(Math.random() * m--);
+//         t = questionArray[m];
+//         questionArray[m] = questionArray[i];
+//         questionArray[i] = t;
+//     }
+//     return questionArray;
+// }
+// console.log(randomFlag(questionArray), "this is shuffled array");
 
-// pick random item from randomized array
-function pickedCountry() {
-    let correctAnswer = Math.floor(Math.random() * questionArray.length)
-    let i = correctAnswer;
-    console.log(questionArray[i], "this is picked one of shuffled array");
-}
-module.exports = {
-    pickedCountry: pickedCountry
-}
-// pickedCountry();
+// // pick random item from randomized array
+// function pickedCountry() {
+//     let correctAnswer = Math.floor(Math.random() * questionArray.length)
+//     let i = correctAnswer;
+//     console.log(questionArray[i], "this is picked one of shuffled array");
+// }
+// module.exports = {
+//     pickedCountry: pickedCountry
+// }
+// // pickedCountry();
 
 // calling random item
 // const q1 = demoQuestion(questionArray);
