fef631d3052d783b8b8724dcc36200bd7d45cc95
kys3923 <kys3923@gmail.com>
2021-01-29 01:04:08 -0500

    finished framework, starting styling

--- routes/scores.js
+++ routes/scores.js
@@ -6,9 +6,32 @@ const isLoggedIn = require('../middleware/isLoggedIn');
 const { sequelize } = require('../models');
 
 // game result
-router.get('/score', isLoggedIn, (req, res) => {
-    res.render('scores/scoreindex.ejs');
+router.get('/comment', isLoggedIn, (req, res) => {
+    db.comment.findAll({
+        order: [['updatedAt', 'DESC']]
+    })
+    .then(foundComment => {
+        res.render ('scores/comment.ejs', {foundComment});
+    })
+    .catch(err => {
+        console.log(err);
+    })
 });
+
+router.post('/comment', isLoggedIn, (req, res) => {
+    db.comment.findOrCreate({
+        where: {
+            name: req.user.dataValues.name,
+            userId: req.user.dataValues.id,
+            comment: req.body.comment
+    }})
+    .then(createdPost => {
+        res.redirect('comment');
+    }).catch(err => {
+        console.log(err);
+    })
+});
+
 
 // ranking
 router.get('/ranking', (req, res) => {
